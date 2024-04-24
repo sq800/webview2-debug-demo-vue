2930df23e11e7eecf90ca8b6b0fa2c27fb240680
yangsq <ysq@systech.com.cn>
2024-04-24 10:45:26 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{ msg: string }>()
+console.log("come in");
+const openFile = async () => {
+  const a = await window.chrome.webview.hostObjects.fileController.OpenFileDialog();
+}
+const saveFile = async () => {
+  const a = await window.chrome.webview.hostObjects.fileController.SaveFileDialog();
+}
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="openFile">打开文件</button>
+  <button @click="saveFile">保存文件</button>
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
